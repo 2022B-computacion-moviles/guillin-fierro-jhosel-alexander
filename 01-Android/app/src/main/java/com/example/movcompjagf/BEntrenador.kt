a4d0ab1e3f7f845aec294ecb4e1f178c15984d16
AlexanderG1999 <95202925+AlexanderG1999@users.noreply.github.com>
2023-01-09 12:07:01 -0500

    Clase 13

--- 01-Android/app/src/main/java/com/example/movcompjagf/BEntrenador.kt
+++ 01-Android/app/src/main/java/com/example/movcompjagf/BEntrenador.kt
@@ -1,5 +1,7 @@
 package com.example.movcompjagf
 
 class BEntrenador(val nombre: String, val descripcion: String) {
-
+    override fun toString(): String {
+        return "${nombre} - ${descripcion}"
+    }
 }
