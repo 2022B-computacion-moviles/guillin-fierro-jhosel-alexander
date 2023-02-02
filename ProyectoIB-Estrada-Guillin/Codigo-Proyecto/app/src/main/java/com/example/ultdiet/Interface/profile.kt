3e38a9e3794d47d914c97f0d2cb565c94a76f6ca
AlexanderG1999 <95202925+AlexanderG1999@users.noreply.github.com>
2023-02-02 11:29:42 -0500

    First commit

---
+++ ProyectoIB-Estrada-Guillin/Codigo-Proyecto/app/src/main/java/com/example/ultdiet/Interface/profile.kt
@@ -0,0 +1,12 @@
+package com.example.ultdiet.Interface
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ultdiet.R
+
+class profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
